50a66d9a92f176d088cf425b66ae67bd899baaf9
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-30 15:26:29 +0400

    Q.45

--- src/main/java/com/nidhal/scheduleTask/Schedule.java
+++ src/main/java/com/nidhal/scheduleTask/Schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "0 0 12 1 1,4,7,10 *")
+    @Scheduled(cron = "0 0 8 1-29 * *")
     public void scheduleTask(){
         System.out.println("Hello World :)");
     }
